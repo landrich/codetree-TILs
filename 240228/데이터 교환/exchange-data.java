31f7547a717607614e78b0917d3b1f9996ece893
landrich <153581650+landrich@users.noreply.github.com>
2024-02-28 16:11:40 +0900

    Title: 데이터 교환, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240228/데이터 교환/exchange-data.java
+++ 240228/데이터 교환/exchange-data.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7, temp;
-        temp = a;
+        temp = b;
+        b = a;
         a = c;
-        c = b;
-        b = temp;
-    
+        c = temp;
+        
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
