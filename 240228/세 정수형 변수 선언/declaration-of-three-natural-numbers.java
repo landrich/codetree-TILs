a9023cfef7f46183773acc8b77389297e6eb8987
landrich <153581650+landrich@users.noreply.github.com>
2024-02-28 11:52:25 +0900

    Title: 세 정수형 변수 선언, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240228/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 7, num2 = 23, num3 = 30;
+        System.out.printf("%d + %d = %d", num1, num2, num3);
+    }
+}
