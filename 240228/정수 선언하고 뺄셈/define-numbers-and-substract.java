3efacf0c30ab92b4fc1d34a765facff9c739f8f1
landrich <153581650+landrich@users.noreply.github.com>
2024-02-28 11:38:08 +0900

    Title: 정수 선언하고 뺄셈, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240228/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 97, num2 = 13;
+        System.out.print(num1 + " - " + num2 + " = " + (num1 - num2));  
+    }
+}
