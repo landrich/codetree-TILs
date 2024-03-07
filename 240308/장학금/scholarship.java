3cce94b6131abb987ff1da45b95e584ddcf37382
landrich <153581650+landrich@users.noreply.github.com>
2024-03-08 06:44:07 +0900

    Title: 장학금, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/장학금/scholarship.java
+++ 240308/장학금/scholarship.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int middle = sc.nextInt();
+        int finals = sc.nextInt();
+        
+        if (middle >= 90 && finals >= 95) {
+            System.out.println(100000);
+        } else if (middle >= 90 && finals >= 90) {
+            System.out.println(50000);
+        } else {
+            System.out.println(0);
+        }
     }
 }
