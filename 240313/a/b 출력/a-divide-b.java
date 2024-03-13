5f66c6b48d109ad9e4fb1d848ecea2555cae96a1
landrich <153581650+landrich@users.noreply.github.com>
2024-03-13 14:47:40 +0900

    Title: a/b 출력, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/a/b 출력/a-divide-b.java
+++ 240313/a/b 출력/a-divide-b.java
@@ -5,7 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int n = a;
 
         System.out.print(a > b ? (a / b) + "." + ((a % b) * 10) / b : "0." + (a * 10) / b);
         a = (a * 10) % b;
