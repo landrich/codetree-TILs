7d6da2e9a33c2d05381cf844bedc2d3300c65461
landrich <153581650+landrich@users.noreply.github.com>
2024-03-14 14:38:20 +0900

    Title: 배수의 개수 2, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int k = sc.nextInt();
+            if (k % 2 == 1) {
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
