2c93703b6c72a0726d351cc55ec00eb1014fce78
landrich <153581650+landrich@users.noreply.github.com>
2024-03-14 13:48:18 +0900

    Title: 369 게임, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/369 게임/369-game.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                System.out.print(0 + " ");
+            } else if (i % 10 != 0 && (i % 10) % 3 == 0) {
+                System.out.print(0 + " ");
+            } else if (i / 30 >= 1 && i % 30 <= 9) {
+                System.out.print(0 + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
