54ef9cdf19bee01b149e840f5102ff0db5fe1979
landrich <153581650+landrich@users.noreply.github.com>
2024-03-17 23:06:18 +0900

    Title: 특정 숫자 맞추기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240317/특정 숫자 맞추기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        while (true) {
+            n = sc.nextInt();
+            if (n == 25) {
+                System.out.println("Good");
+                break;
+            } else if (n < 25) {
+                System.out.println("Higher");
+            } else {
+                System.out.println("Lower");
+            }
+        }
+    }
+}
