124d6d507d5962d71b9c7bdce33890fc6b01650d
landrich <153581650+landrich@users.noreply.github.com>
2024-03-18 09:45:25 +0900

    Title: 3n + 1 수열, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240318/3n + 1 수열/3n-plus-1-sequence.java
+++ 240318/3n + 1 수열/3n-plus-1-sequence.java
@@ -7,17 +7,17 @@ public class Main {
         int cnt = 0;
 
         while (true) {
+            if (n == 1) {
+                System.out.println(cnt);
+                break;
+            }
+
             if (n % 2 == 0) {
                 n /= 2;
                 cnt++;
             } else {
                 n = n * 3 + 1;
                 cnt++;
-            }
-
-            if (n == 1) {
-                System.out.println(cnt);
-                break;
             }
         }
     }
