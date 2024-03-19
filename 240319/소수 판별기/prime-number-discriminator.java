1efc36a15c45b842ad06530a9c62acc99cd20656
landrich <153581650+landrich@users.noreply.github.com>
2024-03-19 09:37:32 +0900

    Title: 소수 판별기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) flag = false; 
+        }
+
+        System.out.println(flag ? "P" : "C");
+    }
+}
