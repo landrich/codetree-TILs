9643739c60a99bf89e79e49921cabfaa000439a9
landrich <153581650+landrich@users.noreply.github.com>
2024-03-19 09:18:09 +0900

    Title: 합성수 판별기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/합성수 판별기/factorization-discriminator.java
+++ 240319/합성수 판별기/factorization-discriminator.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         boolean flag = false;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) flag = true;
         }
 
