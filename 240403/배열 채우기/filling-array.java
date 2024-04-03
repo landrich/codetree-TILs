3790ab0dde627bac31c9221a12b86979e2c04924
landrich <153581650+landrich@users.noreply.github.com>
2024-04-03 09:06:15 +0900

    Title: 배열 채우기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240403/배열 채우기/filling-array.java
+++ 240403/배열 채우기/filling-array.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numArr = new int[10];
-        int n = 0;
+        int n, cnt = 9;
 
         for (int i = 0; i < 10; i++) {
             n = sc.nextInt();
             if (n == 0) {
-                n = i;
+                cnt = i - 1;
                 break;
             }
             numArr[i] = n;
         }
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = cnt; i >= 0; i--) {
             System.out.print(numArr[i] + " ");
         }
     }
