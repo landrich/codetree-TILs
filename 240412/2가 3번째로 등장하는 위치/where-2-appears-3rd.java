0766543461294ca76b3bae029e859c4aefae530c
landrich <153581650+landrich@users.noreply.github.com>
2024-04-12 13:01:55 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240412/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numArr = new int[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (numArr[i] == 2) {
+                ++cnt;
+                if (cnt == 3) {
+                    System.out.println(i + 1);
+                    break;
+                }
+            }
+        }
+    }
+}
