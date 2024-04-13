873ed1f0441b6f4872a10cd76e37a40a26cb30c9
landrich <153581650+landrich@users.noreply.github.com>
2024-04-13 22:01:14 +0900

    Title: 999 또는 -999, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240413/999 또는 -999/999-or-999.java
+++ 240413/999 또는 -999/999-or-999.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numArr = new int[100];
-        int num;
+        int num, bPoint = 0;
 
         for (int i = 0; i < 100; i++) {
             num = sc.nextInt();
             if (num == 999 || num == -999) {
+                bPoint = i;
                 break;
             }
             numArr[i] = num;
@@ -16,13 +17,13 @@ public class Main {
 
         int max = numArr[0], min = numArr[0];
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < bPoint; i++) {
             if (numArr[i] > max) {
                 max = numArr[i];
             }
         }
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < bPoint; i++) {
             if (numArr[i] < min) {
                 min = numArr[i];
             }
