b8412d3929b7be18b36f7772063d54265d493826
landrich <153581650+landrich@users.noreply.github.com>
2024-05-01 19:54:24 +0900

    Title: 특정 원소들의 합, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] numArr = new int[4][4];
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                numArr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += numArr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
