feae68d93bae4b2aeb1b2c3c509c1bf7df85aea5
landrich <153581650+landrich@users.noreply.github.com>
2024-05-20 09:09:40 +0900

    Title: 문자 개수 세기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/문자 개수 세기/count-char.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ch = sc.next().charAt(0);
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
