65f16b7a65690f948f8d9b91fb43026f29663d40
landrich <153581650+landrich@users.noreply.github.com>
2024-05-21 09:25:57 +0900

    Title: 단어로 구분하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/단어로 구분하기/separate-words-with-words.java
+++ 240521/단어로 구분하기/separate-words-with-words.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] strArr = new String[10];
         String str = sc.nextLine();
 
         for (int i = 0; i < str.length(); i++) {
