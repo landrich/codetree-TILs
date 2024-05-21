c341861b151ac84ecfbb07041d49ee93dd3e1fa2
landrich <153581650+landrich@users.noreply.github.com>
2024-05-22 08:57:51 +0900

    Title: Run Length 인코딩, Time: 134ms, Memory: 11MB, Status: Passed - CodeTree

--- 240522/Run Length 인코딩/run-length-encoding.java
+++ 240522/Run Length 인코딩/run-length-encoding.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int cnt = 1;
+        int cnt = 0;
         char lastChar = str.charAt(0);
         String result = "";
 
-        for (int i = 1; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             if (lastChar == str.charAt(i)) {
                 cnt++;
             } else {
