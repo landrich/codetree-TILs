57f5f8ecc8862dbdf1e0ad02c27b82b02feecbf9
landrich <153581650+landrich@users.noreply.github.com>
2024-05-30 08:56:52 +0900

    Title: 대문자로 출력하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240530/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c;
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                System.out.print(c);
+            } else if (c >= 'a' && c <= 'z') {
+                System.out.print((char)(c - 32));
+            }
+        }
+    }
+}
