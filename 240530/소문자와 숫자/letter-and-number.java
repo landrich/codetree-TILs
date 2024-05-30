1f1898f88b6816631d9ee5cbd192c2da343e2d5b
landrich <153581650+landrich@users.noreply.github.com>
2024-05-30 09:21:20 +0900

    Title: 소문자와 숫자, Time: 236ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240530/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c;
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                System.out.print((char)(c + 32));
+            } else if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+                System.out.print(c);
+            }
+        }
+    }
+}
