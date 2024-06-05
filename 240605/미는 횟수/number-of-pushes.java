a6e63e954feba204edced15894a9a1a1e90f804e
landrich <153581650+landrich@users.noreply.github.com>
2024-06-05 10:22:36 +0900

    Title: 미는 횟수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240605/미는 횟수/number-of-pushes.java
+++ 240605/미는 횟수/number-of-pushes.java
@@ -6,13 +6,17 @@ public class Main {
         String str1 = sc.next();
         String str2 = sc.next();
         int cnt = 0;
+        boolean flag = false;
 
         for (int i = 0; i < str1.length(); i++) {
-            if (str1.equals(str2)) break;
+            if (str1.equals(str2)) {
+                flag = true;
+                break;
+            }
             str1 = str1.substring(str1.length() - 1) + str1.substring(0, str1.length() - 1);
             cnt++;
         }
 
-        System.out.println(cnt);
+        System.out.println(flag ? cnt : -1);
     }
 }
