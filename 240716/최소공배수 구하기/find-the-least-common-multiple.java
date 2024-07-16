a5150e8201d1a85ad17e179c893f213419869573
landrich <153581650+landrich@users.noreply.github.com>
2024-07-16 15:08:28 +0900

    Title: 최소공배수 구하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/최소공배수 구하기/find-the-least-common-multiple.java
+++ 240716/최소공배수 구하기/find-the-least-common-multiple.java
@@ -5,6 +5,7 @@ public class Main {
         for (int i = Math.min(n, m); i <= n * m; i++) {
             if (i % n == 0 && i % m == 0) {
                 System.out.println(i);
+                break;
             }
         }
     }
