bb99e44a8cb89c91221ad0a58e17d3e510e3c2b0
landrich <153581650+landrich@users.noreply.github.com>
2024-07-22 15:58:48 +0900

    Title: 그 계절, 그 날, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/그 계절, 그 날/that-season-that-day.java
+++ 240722/그 계절, 그 날/that-season-that-day.java
@@ -51,10 +51,10 @@ public class Main {
             if (m >= 3 && m <= 5) {
                 System.out.println("Spring");
             }
-            else if (m >= 3 && m <= 5) {
+            else if (m >= 6 && m <= 8) {
                 System.out.println("Summer");
             }
-            else if (m >= 3 && m <= 5) {
+            else if (m >= 9 && m <= 11) {
                 System.out.println("Fall");
             } else {
                 System.out.println("Winter");
