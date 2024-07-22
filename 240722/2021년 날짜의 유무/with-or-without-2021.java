b62e64592761fc0c8029cd83b13896572b014a97
landrich <153581650+landrich@users.noreply.github.com>
2024-07-22 15:31:31 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n1, n2;
+    public static int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static boolean cal(int m, int d) {
+        if (days[m] >= d && 0 < d)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+
+        if (cal(m, d)) 
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
