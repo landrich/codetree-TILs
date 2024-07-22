00ed953f18725e3338fa65c3f39e3ad14f477ef2
landrich <153581650+landrich@users.noreply.github.com>
2024-07-22 10:35:55 +0900

    Title: 사칙연산 함수, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/사칙연산 함수/quadratic-operations-function.java
+++ 240722/사칙연산 함수/quadratic-operations-function.java
@@ -14,13 +14,18 @@ public class Main {
             return "False";
         }
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         char o = sc.next().charAt(0);
         int c = sc.nextInt();
+        String result = calc(a, o, c);
 
-        System.out.println(a + " " + o + " " + c + " = " + calc(a, o, c));
+        if (result == "False") {
+            System.out.println("False");
+        } else {
+            System.out.println(a + " " + o + " " + c + " = " + result);
+        }
     }
 }
