a13096dd5d0e7bf6ad1cc4d865ecfa7c8cfaa670
landrich <153581650+landrich@users.noreply.github.com>
2024-07-22 15:32:48 +0900

    Title: 2021년 날짜의 유무, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/2021년 날짜의 유무/with-or-without-2021.java
+++ 240722/2021년 날짜의 유무/with-or-without-2021.java
@@ -5,10 +5,14 @@ public class Main {
     public static int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public static boolean cal(int m, int d) {
-        if (days[m] >= d && 0 < d)
-            return true;
-        else
+        if (m >= 1 && m <= 12) {
+            if (days[m] >= d && 0 < d)
+                return true;
+            else
+                return false;
+        } else {
             return false;
+        }
     }
 
     public static void main(String[] args) {
